4260350a63ca56e2a85e17dea8897db464686868
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-09-08 10:01:59 +0800

    added the add-to-cart functionality

--- ecommerce-product-page/src/redux/slice/cartSlice.js
+++ ecommerce-product-page/src/redux/slice/cartSlice.js
@@ -1,9 +1,19 @@
-import { createSlice, current } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const getTotalCartItems = (cartArr) => {
+	let totalItems = null;
+
+	cartArr.forEach((item) => {
+		totalItems += item.qty;
+	});
+
+	return totalItems;
+};
 
 const cartSlice = createSlice({
 	name: 'cart',
 	initialState: {
-		totalItems: 0,
+		totalItems: 8,
 		items: [
 			{
 				id: '1',
@@ -11,7 +21,7 @@ const cartSlice = createSlice({
 				name: 'Fall Limited Edition Sneakers',
 				price: 125,
 				qty: 3,
-				totalPrice: 400,
+				totalPrice: 375,
 			},
 			{
 				id: '2',
@@ -19,7 +29,7 @@ const cartSlice = createSlice({
 				name: 'Fall Limited',
 				price: 175,
 				qty: 5,
-				totalPrice: 375,
+				totalPrice: 875,
 			},
 		],
 	},
@@ -28,21 +38,38 @@ const cartSlice = createSlice({
 			const itemDetails = action.payload.item;
 			const itemQty = action.payload.qty;
 
-			// const filteredItem = state.items.filter((item) => {
-			// 	if (item.id === '1') {
-			// 		return item;
-			// 	}
-			// });
+			// Check if item exists in the cart, if exists, get the index
+			const filteredItemIndex = state.items.findIndex((item) => {
+				if (item.id === itemDetails.id) {
+					return item;
+				}
+			});
 
-			// if (filteredItem.length === 0) {
-			// 	state.items.push(itemDetails);
-			// } else {
-			// 	const item = state.items.find((item) => {
-			// 		if (item.id === '1') {
-			// 			return item;
-			// 		}
-			// 	});
-			// }
+			// If it doesn't exist, add it to the cart
+			if (filteredItemIndex === -1) {
+				state.items.unshift({
+					id: itemDetails.id,
+					img: itemDetails.images.thumbnails[0],
+					name: itemDetails.name,
+					price: itemDetails.price.salePrice,
+					qty: itemQty,
+					totalPrice:
+						parseFloat(itemDetails.price.salePrice) * parseInt(itemQty),
+				});
+
+				// Update the total number of items in the cart
+				state.totalItems = getTotalCartItems(state.items);
+			}
+			// If it is already in the array, just add the item's qty to the qty in the cart
+			else {
+				state.items[filteredItemIndex].qty += parseInt(itemQty);
+				state.items[filteredItemIndex].totalPrice =
+					state.items[filteredItemIndex].price *
+					state.items[filteredItemIndex].qty;
+
+				// Update the total number of items in the cart
+				state.totalItems = getTotalCartItems(state.items);
+			}
 		},
 	},
 });
